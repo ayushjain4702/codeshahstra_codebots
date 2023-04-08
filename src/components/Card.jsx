b746c092928adec19182617c08dace7d26881c9c
kevin11-code <kevinharia11@gmail.com>
2023-04-09 01:51:56 +0530

    card changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 // const Card = ({ name, del, description, imageUrl }) => {
-const Card = ({ name, description, imageUrl }) => {
+const Card = ({ name, contact, imageUrl }) => {
   return (
     <div className="flex overflow-hidden rounded-lg bg-white shadow-md">
       <img
@@ -9,10 +9,20 @@ const Card = ({ name, description, imageUrl }) => {
         src={imageUrl}
         alt={name}
       />
-      <div className="p-4">
-        <h3 className="text-gray-900 text-lg font-semibold">{name}</h3>
-        <p className="text-gray-600 mt-2 text-sm">{description}</p>
-        <div className="h-fit w-fit cursor-pointer rounded-lg bg-red-500 p-2 hover:bg-red-400">
+      <div className="relative w-full p-4">
+        <h3 className="text-gray-900 text-lg font-semibold">
+          <span className="text-black">Name : </span>
+          {name}
+        </h3>
+        <p className="text-gray-600 mt-2 text-lg">
+          <span className="text-black">Contact : </span>
+          {contact}
+        </p>
+        <p className="text-gray-600 mt-2 text-lg">
+          <span className="text-black">Address : </span>
+          {contact}
+        </p>
+        <div className="absolute right-3 bottom-3 h-fit w-fit cursor-pointer rounded-lg bg-red-500 p-2 hover:bg-red-400">
           <TrashIcon className="h-5 w-5 text-white" />
         </div>
       </div>
--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -1,7 +1,7 @@
-import { FlagIcon, TrashIcon } from "@heroicons/react/24/solid";
+import { FlagIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 // const Card = ({ name, del, description, imageUrl }) => {
-const ReportCard = ({ name, description, imageUrl }) => {
+const ReportCard = ({ name, contact, imageUrl }) => {
   return (
     <div className="flex overflow-hidden rounded-lg bg-white shadow-md">
       <img
@@ -10,21 +10,27 @@ const ReportCard = ({ name, description, imageUrl }) => {
         alt={name}
       />
       <div className="p-4">
-        <h3 className="text-gray-900 text-lg font-semibold">{name}</h3>
-        <p className="text-gray-600 mt-2 text-sm">{description}</p>
+        <h3 className="text-gray-900 text-lg font-semibold">
+          <span className="text-black">Name : </span>
+          {name}
+        </h3>
+        <p className="text-gray-600 mt-2 text-lg">
+          <span className="text-black">Contact : </span>
+          {contact}
+        </p>
         <div className="mt-5 flex gap-5">
-        <div className="h-fit w-fit cursor-pointer rounded-lg bg-red-500 p-2 hover:bg-red-400">
-          <FlagIcon className="h-5 w-5 text-white " />
-        </div>
-        <div className="h-fit w-fit cursor-pointer rounded-lg bg-orange-500 p-2 hover:bg-orange-400">
-          <FlagIcon className="h-5 w-5 text-white " />
-        </div>
-        <div className="h-fit w-fit cursor-pointer rounded-lg bg-yellow-500 p-2 hover:bg-yellow-400">
-          <FlagIcon className="h-5 w-5 text-white " />
-        </div>
-        <div className="h-fit w-fit cursor-pointer rounded-lg bg-green-500 p-2 hover:bg-green-400">
-          <FlagIcon className="h-5 w-5 text-white" />
-        </div>
+          <div className="h-fit w-fit cursor-pointer rounded-lg bg-red-500 p-2 hover:bg-red-400">
+            <FlagIcon className="h-5 w-5 text-white " />
+          </div>
+          <div className="h-fit w-fit cursor-pointer rounded-lg bg-orange-500 p-2 hover:bg-orange-400">
+            <FlagIcon className="h-5 w-5 text-white " />
+          </div>
+          <div className="h-fit w-fit cursor-pointer rounded-lg bg-yellow-500 p-2 hover:bg-yellow-400">
+            <FlagIcon className="h-5 w-5 text-white " />
+          </div>
+          <div className="h-fit w-fit cursor-pointer rounded-lg bg-green-500 p-2 hover:bg-green-400">
+            <FlagIcon className="h-5 w-5 text-white" />
+          </div>
         </div>
       </div>
     </div>
